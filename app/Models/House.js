900fbc9cae8d97249e2dbe09d59b85b76a6fd2b2
Jackson Schacher <jackson.schacher@gmail.com>
2021-09-01 12:56:18 -0600

    set up

---
+++ app/Models/House.js
@@ -0,0 +1,32 @@
+import { generateId } from "../Utils/generateId";
+
+export class House {
+
+    constructor(houseData) {
+        this.id = houseData.id || generateId()
+        this.address = houseData.address
+        this.year = houseData.year
+        this.bedrooms = houseData.bedrooms
+        this.bathrooms = houseData.bathrooms
+        this.description = houseData.description
+        this.img = houseData.img
+        this.price = houseData.price
+    }
+
+    get CardTemplate() {
+        return /*html*/ `
+        <div class="col-lg-3 mb-4 listing">
+        <div class="card bg-dark">
+            <img src="${this.img}" alt="listing image" class="rounded">
+            <div class="card-body">
+            <h5 class="d-flex justify-content-between">
+                <span>${this.bedrooms} Beds : ${this.bathrooms} Bath</span><hr><span>${this.address}</span>
+                <span>$ ${this.price}</span>
+            </h5>
+            <p>${this.description}</p>
+            </div>
+        </div>
+        </div>
+        `
+    }
+}
